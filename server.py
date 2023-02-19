0c15e043399e9dc87ee4422ba4e2eed965fa9857
rajat_vishwa <74039825+Rajat-Vishwa@users.noreply.github.com>
2023-02-19 22:53:51 +0530

    more docs

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 import sqlite3
 from flask import Flask, redirect
 
-host_url = 'localhost:5000/{}'
+host_url = 'localhost:5000/{}'  # The template for the shortened urls.
 
 app = Flask(__name__)
 
@@ -10,19 +10,19 @@ def home():
     return "URL Shortner"
 
 
-@app.route('/<short_url>')
+@app.route('/<short_url>')  # Get the back-half
 def redirect_to_original(short_url):
-    short_url = host_url.format(short_url)
+    short_url = host_url.format(short_url)  # Convert the back-half to url
 
-    query = f"SELECT long_url FROM urls WHERE short_url = ?"
+    query = f"SELECT long_url FROM urls WHERE short_url = ?"  # Get its corresponding long url.
     cursor.execute(query, (short_url,))
 
     res = cursor.fetchone()
-    if res is not None:
+    if res is not None: 
         long_url = res[0]
-        if long_url.find("http://") != 0 and long_url.find("https://") != 0:
-            long_url = "https://" + long_url
-        return redirect(long_url)
+        if long_url.find("http://") != 0 and long_url.find("https://") != 0:  
+            long_url = "https://" + long_url    # Complete the url if its not.
+        return redirect(long_url)   # Redirect to the destination.
     else:
         return "URL not found"
 
